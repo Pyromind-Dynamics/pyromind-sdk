1fc2d10d16c1f88af580fc5225843b3613ab72fb
will.wangt <wang17484920@gmail.com>
2026-08-03 11:10:34 +0800

    update version 0.1.8

--- pyromind_sdk/__init__.py
+++ pyromind_sdk/__init__.py
@@ -8,7 +8,7 @@ In the real platform runtime environment, nodes should prioritize importing
 base classes from `app.models.nodes`.
 """
 ## todo update_version
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 
 # Export YAML nodes functionality
 from .nodes import (
--- pyromind_sdk/nodes/__init__.py
+++ pyromind_sdk/nodes/__init__.py
@@ -4,7 +4,7 @@ YAML node configuration system
 Provides functionality for loading nodes from YAML configuration files.
 """
 ## todo update_version
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 
 from .yaml_loader import (
     load_nodes_from_yaml,
