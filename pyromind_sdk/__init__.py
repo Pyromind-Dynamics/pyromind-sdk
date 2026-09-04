59d28faedd3b18c89ee54807c9af9bbdbbfdd620
will.wangt <wang17484920@gmail.com>
2026-09-04 15:56:25 +0800

    fix

--- pyromind_sdk/__init__.py
+++ pyromind_sdk/__init__.py
@@ -8,7 +8,7 @@ In the real platform runtime environment, nodes should prioritize importing
 base classes from `app.models.nodes`.
 """
 ## todo update_version
-__version__ = "0.1.10"
+__version__ = "0.1.11rc1"
 
 # Export YAML nodes functionality
 from .nodes import (
